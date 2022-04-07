2a7815513dc49a34d17fb63f4167c51dff1325c3
Mark Murphy <murphy.mark@northeastern.edu>
2022-04-07 17:38:05 -0400

    Add CircleListing component

--- src/components/CircleListing/CircleListing.tsx
+++ src/components/CircleListing/CircleListing.tsx
@@ -1,4 +1,13 @@
+import { ReactComponent as CircleSVG } from '../../resources/circle.svg';
+import styles from './CircleListing.module.css';
+
 const CircleListing = () => {
-  return <></>;
+  return (
+    <div className={styles['circle-listing']}>
+      <CircleSVG className={styles.circle} />
+      <h2>Red</h2>
+      <p>$24.99</p>
+    </div>
+  );
 };
 export default CircleListing;
